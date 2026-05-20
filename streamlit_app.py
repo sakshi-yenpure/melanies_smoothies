d94580b5cd2dc19a9968365320e082415b0eaffa
Sakshi Yenpure <sakshisyenpure@gmail.com>
2026-05-20 10:43:51 +0530

    Refactor smoothie order logic and clean up code
    Refactor smoothie order submission and remove unused code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,11 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
+
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """INSERT INTO smoothies.public.orders (ingredients, name_on_order)
             VALUES ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -35,7 +38,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
